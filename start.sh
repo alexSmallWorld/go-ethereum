d684b956c423a9c5dc29326c499da07519f37010
alexSmallWorld <chenwei01118@qq.com>
2022-03-04 18:50:43 +0800

    chainid 304

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-./build/bin/geth --log.debug --verbosity 5 --datadir ./data.dev  --http console --dev --allow-insecure-unlock --http.corsdomain '*'
+#./build/bin/geth --log.debug --verbosity 5 --datadir ./data.dev  --http console --dev --allow-insecure-unlock --http.corsdomain '*'
+./build/bin/geth --networkid 304 --log.debug --verbosity 5 --datadir ./data.dev2  --http console --dev --allow-insecure-unlock --http.corsdomain '*'
