907a24fda3dde7942981e0ea71125216529954da
alexSmallWorld <chenwei01118@qq.com>
2022-03-04 18:55:16 +0800

    tmp

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #./build/bin/geth --log.debug --verbosity 5 --datadir ./data.dev  --http console --dev --allow-insecure-unlock --http.corsdomain '*'
-./build/bin/geth --networkid 304 --log.debug --verbosity 5 --datadir ./data.dev2  --http console --dev --allow-insecure-unlock --http.corsdomain '*'
+./build/bin/geth --networkid 304 --log.debug --verbosity 5 --datadir ./data.dev2  --http console --dev --allow-insecure-unlock --http.corsdomain '*' --http.api debug,eth,web3,personal,net,admin,miner,txpool
